136ca4d7690ee9ee745aa94938ba9c109bedd2f0
zjw <zhangjw@tsinglink.com>
2021-04-23 16:27:48 +0800

    migrate to androidx

--- libuvccamera/src/main/java/com/serenegiant/common/BaseActivity.java
+++ libuvccamera/src/main/java/com/serenegiant/common/BaseActivity.java
@@ -29,9 +29,9 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.support.annotation.NonNull;
-import android.support.annotation.StringRes;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
